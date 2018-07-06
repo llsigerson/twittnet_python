0f4ca4cce005a312e4ecaeaf9d95a68d0552cd41
llsigerson <llsigerson@gmail.com>
2018-07-06 16:22:23 +0800

    updating initial functions

--- get_mentionees.py
+++ get_mentionees.py
@@ -1,2 +1,15 @@
-print("Hello world!!")
-lefieofjew
+
+def get_mentionees(tweets):
+    mentionees= dict()
+    for entities in tweets["entities"]:
+        #get all users mentioned in the tweet
+        if len(entities["user_mentions"])>0:
+            names = [entry["id_str"] for entry in entities["user_mentions"]]
+            #update the mentionees dictionary with these names
+            for name in names:
+                if name in mentionees.keys():
+                    mentionees[name]+=1
+                else:
+                    mentionees[name]=1
+    return (mentionees)
+

a8ff0dd3d0f600b5e6c08e4d183b50d4ef053403
llsigerson <llsigerson@gmail.com>
2018-11-04 15:41:25 +0800

    ongoing updates

--- get_tweets.py
+++ get_tweets.py
@@ -1,6 +1,14 @@
+
+import pytz
 import datetime
-def get_tweets(user, startday = datetime.date.today()-datetime.timedelta(days=30), stopday= datetime.date.today()):
-    tweets= tweepy_to_df(api.user_timeline(user))
+
+
+def get_tweets(user, startday = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)-datetime.timedelta(days=30), stopday= datetime.datetime.utcnow().replace(tzinfo=pytz.utc)):
+
+    tweets= tweepy_to_df(api.user_timeline(user, count=200))
+    #check if oldest tweet is still younger than our startday
+    #while tweets.created_at[-1]>startday:
+
     return(tweets)
 
 tweets=get_tweets("jonfavs")
